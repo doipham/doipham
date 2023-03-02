f68aaa92a4e166bdc5f4891ab4998d3a1fc5388c
doipham <pngocdoi@gmail.com>
2023-03-02 11:01:38 +0700

    fix

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,14 +1,14 @@
 const siteMetadata = {
-  appName: "ON Plus",
-  title: "ON Plus - Xem trực tiếp bóng đá, thể thao, tin tức số 1 Việt Nam",
+  appName: "ColorLess",
+  title: "ColorLess - Thời trang hàng đầu Việt Nam",
   author: "Mise.ai",
   headerTitle: "DeFi for home",
   description:
-    "ON Plus tin tức thể thao mới nhất - Thông tin nhanh & chính xác được cập nhật hàng giờ. Đọc báo thể thao online Việt Nam & Thế giới nóng nhất trong ngày về thể thao,...",
-  siteLogo: "/siteLogo.png",
+    "ColorLess tin tức thể thao mới nhất - Thông tin nhanh & chính xác được cập nhật hàng giờ. Đọc báo thể thao online Việt Nam & Thế giới nóng nhất trong ngày về thể thao,...",
+  siteLogo: "https://brandmark.io/logo-rank/random/bp.png",
   siteUrl: "https://mise.ai",
   theme: "light", // system, dark or light
-  thumb: "https://stvinaprod.vtvcab.vn/6ba1de50-b726-4675-a372-3df496440086.png"
+  thumb: "https://brandmark.io/logo-rank/random/bp.png"
 }
 
 export default siteMetadata
